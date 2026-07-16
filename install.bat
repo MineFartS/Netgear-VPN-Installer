1d633db76f60b18bc3e78f737328683a8bf56add
Phil Hunt <philiphuntiv2@gmail.com>
2026-07-16 17:20:31 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -15,7 +15,9 @@ start /wait msiexec.exe /i "OpenVPN-2.5.5-I602-amd64.msi" /qn /norestart /passiv
 :: Copy Configuration
 xcopy "config\*.*" "C:\Program Files\OpenVPN\config" /Y
 
-:: Rename Network Adapter
-powershell -Command "Get-NetAdapter | Where-Object InterfaceDescription -like 'TAP*V9' | Rename-NetAdapter -NewName 'NETGEAR-VPN' -ErrorAction SilentlyContinue"
+powershell -Command ^
+    "$Adapter = Get-NetAdapter | Where-Object InterfaceDescription -like '*TAP*V9*';" ^
+    "$Adapter | Rename-NetAdapter -NewName 'NETGEAR-VPN';" ^
+    "$Adapter | Set-NetIPInterface -InterfaceMetric 1;"
 
 pause
