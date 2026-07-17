d3c964f65ee7b7e9b77053b2d2cad2a568aa0391
Phil Hunt <philiphuntiv2@gmail.com>
2026-07-17 06:01:32 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,14 +10,24 @@ if %errorLevel% neq 0 (
 cd /d "%~dp0"
 
 :: Install OpenVPN
-start /wait msiexec.exe /i "OpenVPN-2.5.5-I602-amd64.msi" /qn /norestart /passive
+start /wait msiexec.exe /i ^
+    "OpenVPN-2.5.5-I602-amd64.msi" ^
+    ADDLOCAL=OpenVPN.Service,OpenVPN.PLAP.Register,Drivers.TAPWindows6,OpenVPN ^
+    /qn /norestart /passive
 
 :: Copy Configuration
-xcopy "config\*.*" "C:\Program Files\OpenVPN\config" /Y
+xcopy "config\*.*" "C:\Program Files\OpenVPN\config\" /Y
 
+:: Configure interface adapter
 powershell -Command ^
     "$Adapter = Get-NetAdapter | Where-Object InterfaceDescription -like '*TAP*V9*';" ^
     "$Adapter | Rename-NetAdapter -NewName 'NETGEAR-VPN';" ^
     "$Adapter | Set-NetIPInterface -InterfaceMetric 1;"
+
+:: Configure OpenVPN Service
+powershell -Command ^
+    "$service = -name '*openvpn*';" ^
+    "$service | Set-Service -StartupType Automatic;" ^
+    "$service | Restart-Service -Force;"
 
 pause
